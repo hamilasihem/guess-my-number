0108e710a79e0d3c3947399b0c20455b7ab6ac9c
Hamila Sihem <sihemhamila90@gmail.com>
2025-02-22 00:12:32 +0100

    ajout de la fonction Again()

--- starter/script.js
+++ starter/script.js
@@ -4,8 +4,12 @@ const msg = document.querySelector(".message");
 const score1 = document.querySelector(".score");
 const score2 = document.querySelector(".highscore");
 const div = document.querySelector(".number");
+const btnAgain = document.querySelector(".again");
+
 boutonCheck.addEventListener("click", handelClick);
+
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
 let scoreValue = 20;
 let highscoreValue = 0;
 function handelClick() {
@@ -25,6 +29,16 @@ function handelClick() {
     }
   } else {
     msg.textContent = "game over!";
-    document.body.style.background = "#DC0A0AFF";
+    document.body.style.background = "#079407FF";
   }
+}
+btnAgain.addEventListener("click", Again);
+function Again() {
+  msg.textContent = "Start guessing...";
+  inputNumber.value = "";
+  score1.textContent = 20;
+  score2.textContent = 0;
+  div.textContent = "?";
+  document.body.style.backgroundColor = "#222";
+  let secretNumber = Math.trunc(Math.random() * 20) + 1;
 }
